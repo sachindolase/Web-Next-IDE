b3229e9e7164009d8f10543bf3c5d4c647aa671e
sachindolase <sachindolase@gmail.com>
2024-05-15 18:56:40 +0530

    ide

---
+++ hooks/useCodes.jsx
@@ -0,0 +1,13 @@
+const { useState } = require("react");
+
+const useCodes = () => {
+    const [codes, setCodes] = useState("");
+    
+    const setCodesValue = (value) => {
+        setCodes(value);
+    }
+    return [ codes, setCodesValue ];
+}
+
+export default useCodes;
+
